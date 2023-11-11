4552e244fff516bfa908aacbef9b186fd7747885
elngbr <eftimieelena22@stud.ase.ro>
2023-11-11 17:30:43 +0200

    First class finished with std accesor plus ruleof3 implemented. Second class methods started. To be continued

--- doloremipsumcpp/doloremipsumcpp/EventLocation.h
+++ doloremipsumcpp/doloremipsumcpp/EventLocation.h
@@ -3,15 +3,132 @@
 #include <iostream>
 #include <string>
 using namespace std;
-enum typeOfTicket{VIP, LAWN, TRIBUNE, BOXES};
+enum typeOfTicket{VIP, LAWN, TRIBUNE, BOXES, STAND}; ///also type of zone
+enum typeOfEvent{CONCERT, FOOTBALL, THEATRE, FILM, CHARITY, FAIR};
+
 class EventPlace {
 private:
-	int maximumSeats=0;
-	int noRows=0;
-	int noZones=0;
-	string* matrixOfSeats=nullptr;
+	int noRows = 0;
+	int noZones = 0;
+	//typeOfTicket;
+	int noSeatsPerRow = 0;
+	int** stateOfSeats = nullptr;
 
 public:
+	int static const MAX_SEATS_ALLOWED = 1000;
+	int static const MIN_ZONES = 1;
+	int static const MIN_ROWS_PER_ZONE = 5;
+	int static const MIN_SEATS_PER_ROW = 10;
+
+
+	int getNumberOfRows()   ///if I create a setter for this one I risk to have someone modifying it and adding a number of rows which is less than my actual number
+	{
+		return this->noRows;
+	}
+
+	int getNoOfSeatsPerRows()   ///if I create a setter for this one I risk to have someone modifying it and adding a number of rows which is less than my actual number
+	{
+		return this->noSeatsPerRow;
+	}
+
+
+	int getNumberOfZones()
+	{
+		return this->noZones;
+	}
+
+	void setNumberofZones(int auxNumber)
+	{
+		if (auxNumber < EventPlace::MIN_ZONES)
+		{
+			throw exception("You can't have no zone!");
+		}
+	}
+
+	int** getStateOfSeats()
+	{
+		int** copy = new int* [this->noRows];
+		for (int i = 0; i < noRows; i++) {
+			copy[i] = new int[this->noSeatsPerRow];
+
+			for (int j = 0; j < noSeatsPerRow; j++)
+				copy[i][j] = this->stateOfSeats[i][j];
+		}
+
+		return copy;
+	}
+
+
+	void setStateOfSeats(int** matrix, int auxNoRows, int auxNoSeatsPerRow) {
+		if (matrix == nullptr || auxNoRows < EventPlace::MIN_ROWS_PER_ZONE || auxNoSeatsPerRow < EventPlace::MIN_SEATS_PER_ROW) {
+			throw exception("Wrong value!");
+		}
+
+
+		for (int i = 0; i < noRows; i++) {
+			delete[] stateOfSeats[i];
+		}
+		delete[] stateOfSeats;
+
+		stateOfSeats = new int* [auxNoRows];
+		for (int i = 0; i < auxNoRows; i++) {
+			stateOfSeats[i] = new int[auxNoSeatsPerRow];
+		}
+
+
+		for (int i = 0; i < auxNoRows; i++) {
+			for (int j = 0; j < auxNoSeatsPerRow; j++) {
+				stateOfSeats[i][j] = matrix[i][j];
+			}
+		}
+
+
+		noRows = auxNoRows;
+		noSeatsPerRow = auxNoSeatsPerRow;
+	}
+
+	EventPlace()
+	{
+
+	}
+
+	EventPlace(int auxNoRows, int auxNoSeatsPerRow, int** auxMatrix)
+	{
+		this->setStateOfSeats(auxMatrix, auxNoRows, auxNoSeatsPerRow);
+	}
+
+	~EventPlace()
+	{
+
+		for (int i = 0; i < noRows; i++) {
+			delete[] stateOfSeats[i];
+		}
+		delete[] stateOfSeats;
+	}
+
+		
+	EventPlace(const EventPlace & place) 
+		{
+			this->noRows = place.noRows;
+			this->noSeatsPerRow = place.noSeatsPerRow;
+			this->noZones = place.noZones;
+
+			
+			this->stateOfSeats = new int* [noRows];
+			for (int i = 0; i < noRows; i++) {
+				this->stateOfSeats[i] = new int[noSeatsPerRow];
+			}
+
+					for (int i = 0; i < noRows; i++) {
+				for (int j = 0; j < noSeatsPerRow; j++) {
+					this->stateOfSeats[i][j] = place.stateOfSeats[i][j];
+				}
+			}
+	}
+
+
+	
+
 };
 
 
@@ -27,6 +144,23 @@ private:
 
 public:
 
+
+	Event()
+	{
+
+	}
+
+	Event() {
+
+	}
+
+	~Event()
+	{
+		delete[] this->nameOfEvent;
+
+		Event::NO_OF_EVENTS--;
+	}
+
 };
 
 
@@ -36,5 +170,6 @@ private:  //could be by default
 	int static NO_OF_TICKETS;
 	int ticketId = 0;
 	typeOfTicket type = typeOfTicket::BOXES;
+
 
 };
